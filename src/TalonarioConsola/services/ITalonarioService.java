f349c911aa2d20ae3b54f49e5c83f8717ed62743
sNero <bryanmn62@gmail.com>
2026-03-07 12:17:35 -0600

    Ultimos Cambios 07/03 Lab

--- src/TalonarioConsola/services/ITalonarioService.java
+++ src/TalonarioConsola/services/ITalonarioService.java
@@ -1,10 +1,13 @@
 package TalonarioConsola.services;
 
+import java.util.List;
+
 import TalonarioConsola.model.Talonario;
 
 public interface ITalonarioService {
-	public void guardar(Talonario talonario);
+	public boolean guardar(Talonario talonario);
 	public Talonario recuperar(Talonario talonario); //Recuperar talonario en base al objeto dentro del objeto esta el carnet
+	public List<Talonario> recuperarTalonarios();
 	public void modificar(Talonario talonario);
 	public void eliminar(Talonario talonario);
 	
--- src/TalonarioConsola/services/TalonarioServiceImpl.java
+++ src/TalonarioConsola/services/TalonarioServiceImpl.java
@@ -1,30 +1,43 @@
 package TalonarioConsola.services;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import TalonarioConsola.model.Talonario;
 
 public class TalonarioServiceImpl implements ITalonarioService{
 
+	
+	//Comando Lista
+	List<Talonario> talonarios = null;
+
+	public TalonarioServiceImpl() {
+		talonarios = new LinkedList<>();
+	}
 	@Override
-	public void guardar(Talonario talonario) {
-		// TODO Auto-generated method stub
+	public boolean guardar(Talonario talonario) {
+		talonarios.add(talonario);
+		return true;
 		
 	}
-
 	@Override
 	public Talonario recuperar(Talonario talonario) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	@Override
 	public void eliminar(Talonario talonario) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
+	@Override 
 	public void modificar(Talonario talonario) {
 		// TODO Auto-generated method stub
 		
+	}
+	@Override
+	public List<Talonario> recuperarTalonarios() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
