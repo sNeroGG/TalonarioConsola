0617628d69765ceb23e3efd04bd1dd624b637127
sNero <bryanmn62@gmail.com>
2026-03-07 10:50:04 -0600

    Prueba commit 01

---
+++ src/TalonarioConsola/TalonarioConsola.java
@@ -0,0 +1,10 @@
+package TalonarioConsola;
+
+public class TalonarioConsola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+}
