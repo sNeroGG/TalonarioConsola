29ad00639fdda5a389faaf0b93bf37f5bd26212a
sNero <bryanmn62@gmail.com>
2026-03-14 12:37:37 -0600

    Conexion Exitosa Java - SQL

--- src/TalonarioConsola/TalonarioConsola.java
+++ src/TalonarioConsola/TalonarioConsola.java
@@ -1,10 +1,26 @@
 package TalonarioConsola;
 
+import java.sql.Connection;
+import TalonarioConsola.conexion.ConexionMySql;
+
 public class TalonarioConsola {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
-	}
+    public static void main(String[] args) {
 
+        ConexionMySql conexion = new ConexionMySql();
+        Connection con = null;
+
+        try {
+            con = conexion.getConexion();
+
+            if (con != null) {
+                System.out.println("Conectado correctamente a MySQL");
+            } else {
+                System.out.println("No se pudo conectar");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
--- src/TalonarioConsola/conexion/ConexionMySql.java
+++ src/TalonarioConsola/conexion/ConexionMySql.java
@@ -1,35 +1,35 @@
+package TalonarioConsola.conexion;
+
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConexionMySql {
-
-    private final String user = "vroot"; //usuario
-    private final String password = "0000"; //password
-    private final String url = "jdbc:mysql://127.0.0.1:3306/talonario"; //BDD
-    private final String driver = "com.mysql.cj.jdbc.Driver"; //Driver de MySQL
-
+    private final String user = "vroot";
+    private final String password = "0000";
+    private final String url = "jdbc:mysql://127.0.0.1:3306/talonario";
+    private final String driver = "com.mysql.cj.jdbc.Driver";
     private Connection con = null;
 
-    public Connection getConexion() throws SQLException{
-        try{
+    public Connection getConexion() throws SQLException {
+        try {
             Class.forName(driver);
             con = DriverManager.getConnection(this.url, this.user, this.password);
             System.out.println("Conexion exitosa");
-        } catch (ClassNotFoundException | SQLException e){
-            System.out.println("Error en la conexion: " + e);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
         }
         return con;
     }
 
-    public void close (){
-        try{
-            if(con != null){
+    public void close() {
+        try {
+            if (con != null) {
                 con.close();
             }
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(ConexionMySql.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
