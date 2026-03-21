d072be25fd54c6c1e86de7bcb529af6da72c2df7
sNero <bryanmn62@gmail.com>
2026-03-21 11:08:32 -0600

    Primer commit del Parcial 2

--- src/TalonarioConsola/services/TalonarioServiceImpl.java
+++ src/TalonarioConsola/services/TalonarioServiceImpl.java
@@ -1,8 +1,13 @@
 package TalonarioConsola.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import TalonarioConsola.conexion.ConexionMySql;
 import TalonarioConsola.model.Talonario;
 
 public class TalonarioServiceImpl implements ITalonarioService{
@@ -16,16 +21,36 @@ public class TalonarioServiceImpl implements ITalonarioService{
 	}
 	@Override
 	public boolean guardar(Talonario talonario) {
+		boolean hecho = false;
+		ConexionMySql conexion = new ConexionMySql();
+		Connection con = null;
+		PreparedStatement ps;
+		String slq "INSERT INTO estudiante(carnet,nombres,apellidos) values(?,?,?)";
+		try{
+			con = conexion.getConexion();
+			ps = con.prepareStatement(slq);
+			ps.setString(1, talonario.getCarnet());
+
+		} finally {
+			try{
+				con.close();
+				catch (SQLException e){
+					e.printStackTrace();
+				}
+			}
+			return hecho
+		}
+		
 		talonarios.add(talonario);
 		return true;
 		
 	}
 	@Override
 	public Talonario recuperar(Talonario talonario) {
-		
-		
-		return talonarios.get(0);
+		Talonario tal = new Talonario();
+		return tal;
 	}
+
 	@Override
 	public void eliminar(Talonario talonario) {
 		// TODO Auto-generated method stub
@@ -39,6 +64,7 @@ public class TalonarioServiceImpl implements ITalonarioService{
 	@Override
 	public List<Talonario> recuperarTalonarios() {
 		// TODO Auto-generated method stub
+		List<Talonario>talonarios 
 		return talonarios;
 	}
 }
